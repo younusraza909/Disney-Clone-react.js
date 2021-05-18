0a11b6cef54a36f1d75a724cd27d737f684d8163
Muhammad Younus Raza <younusraza909@gmail.com>
2021-05-18 08:16:03 +0500

    Started Home Section

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,6 +42,7 @@ const NavMenu = styled.div`
         bottom: -6px;
         opacity: 0;
         transform: scaleX(0);
+        transform-origin: left center;
         transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
       }
     }
