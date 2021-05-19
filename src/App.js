60138446ce064f016a0b801e48cdf5d84ce1b843
Muhammad Younus Raza <younusraza909@gmail.com>
2021-05-19 22:47:39 +0500

    Added react router + detail page title with Button Section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/detail' exact component={Detail} />
+        </Switch>
+      </Router>
     </div>
   );
 }
