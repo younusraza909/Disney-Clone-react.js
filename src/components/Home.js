ba68a9779fd77659b0565049bf7bce87d8713264
Muhammad Younus Raza <younusraza909@gmail.com>
2021-05-20 08:39:07 +0500

    Added Firebase And Getting Data from it saving it into state using redu toolkit and then using it in movies component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import ImageSlider from "./ImageSlider";
 import Viewers from "./Viewers";
 import Movies from "./Movies";
+import db from "../firebase";
+import { useDispatch } from "react-redux";
+import { setMovies } from "../features/movie/movieSlice";
 
 const Container = styled.main`
   overflow-x: hidden;
@@ -24,6 +27,15 @@ const Container = styled.main`
 `;
 
 const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapshot) => {
+      let tempMovies = snapshot.docs.map((snap) => {
+        return { id: snap.id, ...snap.data() };
+      });
+      dispatch(setMovies(tempMovies));
+    });
+  }, []);
   return (
     <Container>
       <ImageSlider />
